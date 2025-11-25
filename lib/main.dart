1a54b02000a44ee823d33e3163b7da8c009b8dab
liv-febyy <email-github-kamu@example.com>
2025-11-25 17:06:50 +0700

    Fix import provider

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application.MAIN.DART
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
